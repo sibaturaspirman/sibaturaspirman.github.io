73f9efc41a9c99a77ad8a6841c41bd91aec0f163
Asep Irman <asepirman@antigravity.id>
2021-08-25 10:54:15 +0700

    push

---
+++ precache-manifest.26c9e4fc83c7cf7150fea140de3f8205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8ae275d19d11561a1f5db569a7372d",
+    "url": "/alterstage/index.html"
+  },
+  {
+    "revision": "3ce55c7b627283c5306e",
+    "url": "/alterstage/static/css/main.0a76face.chunk.css"
+  },
+  {
+    "revision": "6c551055850748ec49cc",
+    "url": "/alterstage/static/js/2.d77ff8ca.chunk.js"
+  },
+  {
+    "revision": "20a6f33d71d567d1fe0dc1c77a1a22e7",
+    "url": "/alterstage/static/js/2.d77ff8ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce55c7b627283c5306e",
+    "url": "/alterstage/static/js/main.0fee00f4.chunk.js"
+  },
+  {
+    "revision": "2a5a43ac500ed136ec1d",
+    "url": "/alterstage/static/js/runtime-main.8759bb07.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.505b38d370e1386d29db808ea4c61650.js"
+  "/alterstage/precache-manifest.26c9e4fc83c7cf7150fea140de3f8205.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alterstage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
